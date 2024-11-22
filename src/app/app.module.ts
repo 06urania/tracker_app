1e3055708383f682483547fa939d3ca5a2ef7676
SrYeti <k.milo.arana@gmail.com>
2024-11-22 16:00:09 -0300

    firebase funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,28 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'tracker-app-8d60a',
+        appId: '1:220266631184:web:5887964b048f468a0d7a95',
+        storageBucket: 'tracker-app-8d60a.firebasestorage.app',
+        apiKey: 'AIzaSyDlUv5CUcnTzeWHPg0J8sDqX_ZoHPnmmno',
+        authDomain: 'tracker-app-8d60a.firebaseapp.com',
+        messagingSenderId: '220266631184',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
