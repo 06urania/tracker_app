5c18f33fbd5050ca32e4cad8ee2a5e525c5001fc
SrYeti <k.milo.arana@gmail.com>
2024-11-24 04:10:03 -0300

    registrar pide nombre

--- src/app/pages/registrar/registrar.page.ts
+++ src/app/pages/registrar/registrar.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../firebase/auth.service';
 import { Router } from '@angular/router';
+import { FirestoreService } from '../../firebase/firestore.service';
 
 @Component({
   selector: 'app-registrar',
@@ -8,19 +9,46 @@ import { Router } from '@angular/router';
   styleUrls: ['./registrar.page.scss'],
 })
 export class RegistrarPage {
+  name: string = '';
   email: string = '';
   password: string = '';
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private firestoreService: FirestoreService
+  ) {}
 
   async register() {
+    if (!this.name || !this.email || !this.password) {
+      alert('Por favor, complete todos los campos');
+      return;
+    }
+
     try {
-      await this.authService.register(this.email, this.password);
+      const userCredential = await this.authService.register(
+        this.email,
+        this.password
+      );
+
+      const userData = {
+        uid: userCredential.user.uid,
+        name: this.name,
+        email: this.email,
+      };
+
+      await this.firestoreService.createUser(userCredential.user.uid, userData);
+
       alert('Usuario registrado con éxito');
-      this.router.navigate(['/home']); // Redirige al home después del registro
-    } catch (error) {
-      console.error(error);
-      alert('Error al registrar el usuario');
+      this.router.navigate(['/home']);
+    } catch (error: any) {
+      console.error('Error durante el registro:', error);
+      if (error.code === 'auth/email-already-in-use') {
+        alert('Este correo ya está registrado');
+      } else {
+        const errorMessage = this.authService.GenerarError(error);
+        alert(errorMessage);
+      }
     }
   }
 }
