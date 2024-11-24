d70695ab45e852226641be3071ef19faa749eefa
SrYeti <k.milo.arana@gmail.com>
2024-11-24 04:18:33 -0300

    environment.prod arreglado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    projectId: 'tracker-app-8d60a',
+    appId: '1:220266631184:web:5887964b048f468a0d7a95',
+    storageBucket: 'tracker-app-8d60a.appspot.com',
+    apiKey: 'AIzaSyDlUv5CUcnTzeWHPg0J8sDqX_ZoHPnmmno',
+    authDomain: 'tracker-app-8d60a.firebaseapp.com',
+    messagingSenderId: '220266631184',
+  },
 };
